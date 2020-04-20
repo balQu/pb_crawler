d7de415328668f2e03f8ebf8dccee7dc9801ac51
Tobi <balqu@balqu.com>
2020-04-21 00:15:32 +0200

    implemented parser
    First version of a primitive specialized Pastebin/archive-HTML parser

--- pb_crawler/parser.h
+++ pb_crawler/parser.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+struct paste_data
+{
+	std::string id;
+	std::string title;
+	std::string elapsed_time;
+	std::string paste_language;
+};
+
+class Parser
+{
+private:
+	std::vector<paste_data> parsed_data;
+	std::stringstream html_stream;
+public:
+	Parser(){}
+	Parser(std::stringstream stream) : html_stream(std::move(stream)) {}
+	std::vector<paste_data> getParsed_data() { return parsed_data; }
+	void setHtmlStream(std::stringstream stream) { html_stream = std::move(stream); }
+	bool parse();
+};
