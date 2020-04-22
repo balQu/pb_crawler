18197de966a1f212503919e7497f597f4f0ce891
Tobi <tobias.doecke@gmx.de>
2020-04-22 23:49:06 +0200

    get raw paste of found pastes

--- pb_crawler/pb_crawler.cpp
+++ pb_crawler/pb_crawler.cpp
@@ -6,17 +6,25 @@
 int main()
 {
 	Crawler crawler{ "https://pastebin.com/archive" };
+	std::vector<paste_data> data;
 
 	try
 	{
 		Parser parser{ crawler.crawl() };
 		if (parser.parse())
 		{
-			std::vector<paste_data> data = parser.getParsed_data();
+			data = parser.getParsed_data();
 		}
 	}
 	catch (const std::exception& exc)
 	{
 		exc.what();
+	}
+
+	std::vector<std::stringstream> raw_pastes;
+	for (const auto& d : data)
+	{
+		crawler.setUrl("https://pastebin.com/raw/" + d.id);
+		raw_pastes.emplace_back(crawler.crawl());
 	}
 }
