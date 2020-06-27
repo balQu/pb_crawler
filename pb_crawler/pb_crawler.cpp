ceaac1360d335480e56882fe19c7811019a94b99
Tobi <tobias.doecke@gmail.com>
2020-06-27 22:21:18 +0200

    added execution loop

--- pb_crawler/pb_crawler.cpp
+++ pb_crawler/pb_crawler.cpp
@@ -2,34 +2,54 @@
 #include "parser.h"
 
 #include <iostream>
+#include <unordered_set>
+#include <chrono>
+#include <thread>
 
 #define DEBUG
+
+static constexpr int waittime = 60;
+static std::unordered_set<std::string> visited_ids;
 
 int main()
 {
 	Crawler crawler{ "https://pastebin.com/archive" };
 	std::vector<paste_data> data;
 
-	try
+	while (true)
 	{
-		Parser parser{ crawler.crawl() };
-		if (parser.parse())
-		{
-			data = parser.getParsed_data();
-		}
-	}
-	catch (const std::exception& exc)
-	{
-		exc.what();
-	}
-
-	std::vector<std::stringstream> raw_pastes;
-	for (const auto& d : data)
-	{
-		crawler.setUrl("https://pastebin.com/raw/" + d.id);
-		raw_pastes.emplace_back(crawler.crawl());
 #ifdef DEBUG
-		std::cout << "Added: [" << d.paste_language << "] - " << d.title << " (" << d.elapsed_time << ") /" << d.id << "\n";
+		std::cout << "refreshing...\n";
 #endif // DEBUG
+
+		try
+		{
+			crawler.setUrl("https://pastebin.com/archive");
+			Parser parser{ crawler.crawl() };
+			if (parser.parse())
+			{
+				data = parser.getParsed_data();
+			}
+		}
+		catch (const std::exception& exc)
+		{
+			exc.what();
+		}
+		std::vector<std::stringstream> raw_pastes;
+		for (const auto& d : data)
+		{
+			auto result = visited_ids.find(d.id);
+			if (result != std::end(visited_ids))
+			{
+				continue;
+			}
+			crawler.setUrl("https://pastebin.com/raw/" + d.id);
+			raw_pastes.emplace_back(crawler.crawl());
+			visited_ids.insert(d.id);
+#ifdef DEBUG
+			std::cout << "Added: [" << d.paste_language << "] - " << d.title << " (" << d.elapsed_time << ") /" << d.id << "\n";
+#endif // DEBUG
+		}
+		std::this_thread::sleep_for(std::chrono::seconds{ waittime });
 	}
 }
