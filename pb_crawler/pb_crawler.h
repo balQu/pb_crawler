1c5cf9cd3a9992158dcfb49ddd59f3c4888b98d9
Tobi <33381820+balQu@users.noreply.github.com>
2020-11-03 23:02:55 +0100

    added public interface

---
+++ pb_crawler/pb_crawler.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "crawler.h"
+#include "parser.h"
+
+#include <vector>
+
+struct paste_data_content : public paste_data
+{
+	std::string content{};
+};
+
+class pb_crawler
+{
+private:
+	Crawler crawler{ "https://pastebin.com/archive" };
+	std::vector<paste_data_content> data;
+
+	std::stringstream getPasteContent(const paste_data& d);
+public:
+	std::vector<paste_data_content> crawlPastes();
+};
