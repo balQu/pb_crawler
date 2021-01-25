3c4238a0b19da757cdcbd0f9b6e9a04fd93915b6
Sebastian Becker <mail@sbecker.de.com>
2021-01-25 08:08:52 +0100

    Reformat files

--- pb_crawler/pb_crawler.cpp
+++ pb_crawler/pb_crawler.cpp
@@ -5,37 +5,38 @@
 constexpr auto PasteBinUrlRaw = "https://pastebin.com/raw/";
 constexpr auto PasteBinUrlArchive = "https://pastebin.com/archive";
 
-
 auto pb_crawler::getPasteContent(const paste_data& d) -> std::stringstream
 {
-	crawler.setUrl(PasteBinUrlRaw + d.id);
-	return crawler.crawl();
+  crawler.setUrl(PasteBinUrlRaw + d.id);
+  return crawler.crawl();
 }
-
 
 auto pb_crawler::crawlPastes() -> std::vector<paste_data_content>
 {
+  std::vector<paste_data> foundPastes{};
+  try
+  {
+    crawler.setUrl(PasteBinUrlArchive);
+    Parser parser{ crawler.crawl() };
+    if (parser.parse())
+    {
+      foundPastes = parser.getParsed_data();
+    }
+  }
+  catch (const std::exception& exc)
+  {
+    std::cout << exc.what() << "\n";
+    return {};
+  }
 
-	std::vector<paste_data> foundPastes{};
-	try
-	{
-		crawler.setUrl(PasteBinUrlArchive);
-		Parser parser{ crawler.crawl() };
-		if (parser.parse())
-		{
-			foundPastes = parser.getParsed_data();
-		}
-	}
-	catch (const std::exception& exc)
-	{
-		std::cout << exc.what() << "\n";
-		return {};
-	}
+  for (const auto& p : foundPastes)
+  {
+    data.emplace_back(paste_data_content{ p.id,
+      p.title,
+      p.paste_language,
+      p.elapsed_time,
+      getPasteContent(p).str() });
+  }
 
-	for (const auto& p : foundPastes)
-	{
-		data.emplace_back(paste_data_content{ p.id, p.title, p.paste_language, p.elapsed_time, getPasteContent(p).str() });
-	}
-
-	return data;
+  return data;
 }
