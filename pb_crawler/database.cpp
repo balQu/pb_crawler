c855164514a040a7a5d043bd5b169a108a4f1e75
Sebastian Becker <mail@sbecker.de.com>
2021-01-25 08:00:21 +0100

    Add .clang-format and reformat

--- pb_crawler/database.cpp
+++ pb_crawler/database.cpp
@@ -4,65 +4,70 @@
 
 constexpr int MySqlPort = 33060;
 
-namespace db
+namespace db {
+Database& Database::getInstance()
 {
-	Database& Database::getInstance()
-	{
-		static Database instance{};
-		return instance;
-	}
+  static Database instance{};
+  return instance;
+}
 
-	bool Database::connect() const
-	{
-		if (mysql_real_connect(connection, db_config.host.c_str(),
-			db_config.username.c_str(), db_config.password.c_str(),
-			db_config.database_name.c_str(), MySqlPort, nullptr, 0) == nullptr)
-		{
-			auto error = mysql_error(connection);
-			if (error != nullptr)
-			{
-				std::cerr << error << "\n";
-			}
-			
-			mysql_close(connection);
-			return false;
-		}
-		return true;
-	}
+bool Database::connect() const
+{
+  if (mysql_real_connect(connection,
+        db_config.host.c_str(),
+        db_config.username.c_str(),
+        db_config.password.c_str(),
+        db_config.database_name.c_str(),
+        MySqlPort,
+        nullptr,
+        0)
+      == nullptr)
+  {
+    auto error = mysql_error(connection);
+    if (error != nullptr)
+    {
+      std::cerr << error << "\n";
+    }
 
-	std::string Database::query(const std::string& query) const
-	{
-		if (mysql_query(connection, query.c_str()) != 0)
-		{
-			auto error = mysql_error(connection);
-			if (error != nullptr)
-			{
-				std::cerr << error << "\n";
-			}
-			return "";
-		}
+    mysql_close(connection);
+    return false;
+  }
+  return true;
+}
 
-		MYSQL_RES* result = mysql_store_result(connection);
-		if (!result)
-		{
-			return  "";
-		}
+std::string Database::query(const std::string& query) const
+{
+  if (mysql_query(connection, query.c_str()) != 0)
+  {
+    auto error = mysql_error(connection);
+    if (error != nullptr)
+    {
+      std::cerr << error << "\n";
+    }
+    return "";
+  }
 
-		int num_fields = mysql_num_fields(result);
-		MYSQL_ROW row{};
-		std::string query_result{};
+  MYSQL_RES* result = mysql_store_result(connection);
+  if (!result)
+  {
+    return "";
+  }
 
-		while ((row = mysql_fetch_row(result)))
-		{
-			for (int i = 0; i < num_fields; i++)
-			{
-				if (row[i])
-				{
-					query_result += row[i];
-				}
-			}
-		}
-		mysql_free_result(result);
-		return query_result;
-	}
-} // namespace db
+  int num_fields = mysql_num_fields(result);
+  MYSQL_ROW row{};
+  std::string query_result{};
+
+  while ((row = mysql_fetch_row(result)))
+  {
+    for (int i = 0; i < num_fields; i++)
+    {
+      if (row[i])
+      {
+        query_result += row[i];
+      }
+    }
+  }
+  mysql_free_result(result);
+  return query_result;
+}
+}// namespace db
--- pb_crawler/pb_crawler.h
+++ pb_crawler/pb_crawler.h
@@ -7,16 +7,17 @@
 
 struct paste_data_content : public paste_data
 {
-	std::string content{};
+  std::string content{};
 };
 
 class pb_crawler
 {
 private:
-	Crawler crawler{ "https://pastebin.com/archive" };
-	std::vector<paste_data_content> data{};
+  Crawler crawler{ "https://pastebin.com/archive" };
+  std::vector<paste_data_content> data{};
 
-	std::stringstream getPasteContent(const paste_data& d);
+  std::stringstream getPasteContent(const paste_data& d);
+
 public:
-	std::vector<paste_data_content> crawlPastes();
+  std::vector<paste_data_content> crawlPastes();
 };
