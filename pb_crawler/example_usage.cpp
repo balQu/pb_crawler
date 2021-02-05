9e60c636e5b62833728951895b32ff7ac299f875
Tobi <33381820+balQu@users.noreply.github.com>
2021-02-05 23:03:47 +0100

    remove database from example usage

--- pb_crawler/example_usage.cpp
+++ pb_crawler/example_usage.cpp
@@ -1,4 +1,3 @@
-#include "database.h"
 #include "pb_crawler.h"
 
 #include <algorithm>
@@ -8,70 +7,14 @@
 
 static constexpr std::chrono::seconds waittime{ 60 };
 
-auto findPaste(const std::string& id) -> bool
-{
-	std::string sql_query = "SELECT * FROM pastes WHERE id=\"" + id + "\"";
-	auto result = db::Database::getInstance().query(sql_query);
-	return !result.empty();
-}
-
-auto insertPaste(const paste_data_content& d) -> bool
-{
-	std::string sql_insert = "INSERT INTO pastes VALUES('" + d.id + "', '"
-							 + d.paste_language + "', '" + d.title + "', '"
-							 + d.content + "')";
-	auto result = db::Database::getInstance().query(sql_insert);
-	return !result.empty();
-}
-
-auto addPaste(paste_data_content& p) -> void
-{
-	if (p.content.empty())
-	{
-		return;
-	}
-
-	std::replace(std::begin(p.content), std::end(p.content), '\'', '\"');
-	std::replace(std::begin(p.title), std::end(p.title), '\'', '\"');
-
-	if (findPaste(p.id))
-	{
-		return;
-	}
-
-	if (insertPaste(p))
-	{
-		std::cout << "Added paste (id: " << p.id << ") to database.\n";
-	}
-}
-
 auto main() -> int
 {
-	db::Database::getInstance().setConfig(
-		{ "localhost", "root", "", "pastes" });
-	if (!db::Database::getInstance().connect())
-	{
-		std::cout << "Couldn't connect to the database.\n";
-		return 1;
-	}
-
-	std::cout << "There are currently "
-			  << db::Database::getInstance().query(
-					 "SELECT COUNT(*) from pastes")
-			  << " pastes in the database.\n\n";
-
 	pb_crawler crawler{};
 
 	while (true)
 	{
-#ifdef _DEBUG
-		std::cout << "refreshing...\n";
-#endif// _DEBUG
-
 		std::cout << "fetching recent pastes...\n";
 		auto pastes = crawler.crawlPastes();
-
-		std::for_each(std::begin(pastes), std::end(pastes), addPaste);
 
 		std::cout << "waiting " << waittime.count() << " seconds...\n";
 		std::this_thread::sleep_for(waittime);
